1fad9b1f004fdd535272ed05ad7be6bcafb4c29c
Shaurya_Kharb <228geetakharb@gmail.com>
2021-06-26 16:12:15 +0530

    commit

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -1,47 +1,51 @@
-import * as React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-import { Audio } from 'expo-av';
+import * as React from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Audio } from 'expo-av'
 
 export default class PhonicSoundButton extends React.Component {
-   constructor(props){
-      super(props);
-      this.state = {
-        pressedButtonIndex: '',
-      }
-      
+  constructor(props) {
+    super(props)
+    this.state = {
+      pressedButtonIndex: '',
     }
-  playSound = async soundChunk => {
-    console.log(soundChunk);
+  }
+  playSound = async (soundChunk) => {
+    console.log(soundChunk)
     var soundLink =
       'https://whitehatjrcontent.s3.ap-south-1.amazonaws.com/phones/' +
       soundChunk +
-      '.mp3';
+      '.mp3'
     await Audio.Sound.createAsync(
       {
         uri: soundLink,
       },
-      { shouldPlay: true }
-    );
-  };
+      { shouldPlay: true },
+    )
+  }
   render() {
     return (
       <TouchableOpacity
         style={
           this.props.buttonIndex === this.state.pressButtonIndex
-                      ? [styles.chunkButton, { backgroundColor: 'white' }]
-                      : [styles.chunkButton, { backgroundColor: 'red' }]
-          }
+            ? [styles.chunkButton, { backgroundColor: 'white' }]
+            : [styles.chunkButton, { backgroundColor: 'red' }]
+        }
         onPress={() => {
-          this.setState({ pressButtonIndex: this.props.buttonIndex });
-          this.playSound(this.props.soundChunk);
-        }}>
-        <Text style={
-          this.props.buttonIndex === this.state.pressButtonIndex
-                        ? [styles.displayText, { color: 'red' }]
-                        : [styles.displayText, { color: 'white' }]
-        }>{this.props.wordChunk}</Text>
+          this.setState({ pressButtonIndex: this.props.buttonIndex })
+          this.playSound(this.props.soundChunk)
+        }}
+      >
+        <Text
+          style={
+            this.props.buttonIndex === this.state.pressButtonIndex
+              ? [styles.displayText, { color: 'red' }]
+              : [styles.displayText, { color: 'white' }]
+          }
+        >
+          {this.props.wordChunk}
+        </Text>
       </TouchableOpacity>
-    );
+    )
   }
 }
 
@@ -49,16 +53,16 @@ const styles = StyleSheet.create({
   displayText: {
     textAlign: 'center',
     fontSize: 30,
-    color: 'white'
+    color: 'white',
   },
-  chunkButton:{
+  chunkButton: {
     width: '60%',
     height: 50,
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    borderRadius: 10,
+    borderRadius: 20,
     margin: 5,
-    backgroundColor: 'red'
-  }
-});
+    backgroundColor: 'red',
+  },
+})
